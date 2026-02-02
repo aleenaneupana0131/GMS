d40db8163810eeb2ad1c0be4a26a8cf9d87b4a35
aleenaneupana0131 <aleenaneupane510@gmail.com>
2026-02-02 21:58:38 +0545

    Delete includes directory

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-<hr>
-        <p style="text-align: center; color: #777;">
-            <small>&copy; <?php echo date('Y'); ?> Gym Management System (Academic Project)</small>
-        </p>
-    </div> 
-</body>
-</html>
